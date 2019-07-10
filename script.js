efbe363451112a716816d117a0045ab54ed84a5b
Liam Dawson <liamrdawson@gmail.com>
2019-07-10 21:00:46 +0100

    run code from locally installed bcrypt package

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 let unsecurePlainTextPassword = `password`;
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
+bcrypt.genSalt(saltRounds, function(err, salt) {
+    bcrypt.hash(unsecurePlainTextPassword, salt, function(err, hash) {
+        console.log(hash);
+    });
+});
