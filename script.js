1a026de5833d534d9ca33b34080b7b827529a859
Liam Dawson <liamrdawson@gmail.com>
2019-07-11 20:38:08 +0100

    remove node_modules

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let unsecurePlainTextPassword = `password`;
 
+const colors = require('colors');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 
 bcrypt.genSalt(saltRounds, function(err, salt) {
     bcrypt.hash(unsecurePlainTextPassword, salt, function(err, hash) {
-        console.log(hash);
+        console.log(hash.blue);
     });
 });
